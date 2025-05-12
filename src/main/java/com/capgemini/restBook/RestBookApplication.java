dcc07221698493ebd0a2373b8985973630af6612
Om Gade <omgade189@gmail.com>
2025-05-12 12:23:57 +0530

    2nd Commit

---
+++ src/main/java/com/capgemini/restBook/RestBookApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.restBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBookApplication.class, args);
+	}
+
+}
